c92dd080383398bcaa06a939b6241b2b2b50fa4a
Stig Rune Jensen <stig.r.jensen@uit.no>
2022-01-11 16:22:10 +0100

    Update src

--- src/hello_world.cpp
+++ src/hello_world.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
 int main() {
-    std::cout << "HELLO WORLD! Fixing bug 1" << std::endl;
+    std::cout << "HELLO WORLD! main version" << std::endl;
     return EXIT_SUCCESS;
 }
